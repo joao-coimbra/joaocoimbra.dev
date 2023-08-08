44ef7d19493f9c319f92964907e1b05e45abd2be
João Coimbra <joao.benatti.coimbra@gmail.com>
2023-08-08 17:10:58 -0300

    more highlight

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <main className="grid min-h-screen place-content-center p-24">
-            <h1 className="bg-gradient-to-r from-neutral-800 via-neutral-200 to-neutral-800 bg-clip-text text-center text-4xl font-bold leading-loose text-transparent">
+            <h1 className="bg-gradient-to-r from-neutral-500 via-neutral-200 to-neutral-500 bg-clip-text text-center text-4xl font-bold leading-loose text-transparent">
                 Nothing yet.
             </h1>
         </main>
